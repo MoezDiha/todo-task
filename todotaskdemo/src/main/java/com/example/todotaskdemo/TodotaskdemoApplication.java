e9f8ced23e58b6a5b3932bbc9453200724cadbfe
MoezDiha <moezdiha1919@gmail.com>
2024-09-30 20:48:50 +0200

    Project initialization.

---
+++ todotaskdemo/src/main/java/com/example/todotaskdemo/TodotaskdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todotaskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotaskdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodotaskdemoApplication.class, args);
+	}
+
+}
---
+++ todotaskdemo/src/test/java/com/example/todotaskdemo/TodotaskdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todotaskdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotaskdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
